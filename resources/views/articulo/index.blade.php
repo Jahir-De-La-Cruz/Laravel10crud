846dc8eaff965270eb15f0cf465a61ce1ee6a499
Jahir <soyjahirjesua04@gmail.com>
2024-02-23 20:37:23 -0600

    CRUD en Laravel 10

---
+++ resources/views/articulo/index.blade.php
@@ -0,0 +1,77 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>LISTADO DE ARTICULOS</h1>
+@stop
+
+@section('content')
+    <a href="articulos/create" class="btn btn-primary mb-3">CREAR</a>
+
+    <table id="articulos" class="table table-bordered table-dark table-striped shadow-lg mt-4">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Código</th>
+                <th scope="col">Descripción</th>
+                <th scope="col">Cantidad</th>
+                <th scope="col">Precio</th>
+                <th scope="col">Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+        @foreach ($articulos as $articulo)
+            <tr>
+                <td>{{ $articulo->id }}</td>
+                <td>{{ $articulo->codigo }}</td>
+                <td>{{ $articulo->descripcion }}</td>
+                <td>{{ $articulo->cantidad }}</td>
+                <td>{{ $articulo->precio }}</td>
+                <td>
+                    <form action="{{ route('articulos.destroy', $articulo->id) }}" method="POST">
+                        <a href="/articulos/{{ $articulo->id }}/edit" class="btn btn-info">Editar</a>
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Borrar</button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css">
+@stop
+
+@section('js')
+    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+    <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
+
+    <script>	
+        $(document).ready(function() {
+            $('#articulos').DataTable({
+                "order": [[1, "asc"]],
+                "lengthMenu": [[5,10,50,-1], [5,10,50, "Todos"]],
+                "language": {
+                    "lengthMenu": "Mostrar _MENU_ registros por página",
+                    "info": "Mostrando página _PAGE_ de _PAGES_",
+                    "infoEmpty": "No hay registros disponibles",
+                    "infoFiltered": "(Filtrada de _MAX_ Registros)",
+                    "loadingRecords": "Cargando...",
+                    "processing": "Procesando...",
+                    "search": "Buscar:",
+                    "zeroRecords": "No se encontraron registros coincidentes",
+                    "paginate": {
+                        "next": "Siguiente",
+                        "previous": "Anterior"
+                    },
+                },
+            });
+        });
+    </script>
+@stop
---
+++ resources/views/layouts/plantillabase.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CRUD</title>
+    @yield('css')
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+    
+    <h1 class="bg-primary text-white text-center">CRUD con Laravel 10 y Bootstrap 5</h1>
+
+    <div class="container">
+        @yield('contenido')
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    @yield('js')
+</body>
+</html>
